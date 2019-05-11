47d1ac813b8d57ea79ca2f36aaf855bb51268baf
Yasser Zubair <yasserzubair68@gmail.com>
2019-05-11 15:08:30 +0500

    home containers

---
+++ src/containers/Home/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+// import UserRepos from './containers/UserRepos';
+import './style.scss';
+
+class Home extends Component {
+	render() {
+		return (
+			<div id="home">
+				<Container>
+					<h2 className="text-center">Home</h2>
+				</Container>
+			</div>
+		);
+	}
+}
+
+export default Home;
