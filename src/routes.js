dad5285f6dfbcefb151d7aaf206070425a1d6545
Yasser Zubair <yasserzubair68@gmail.com>
2019-05-11 15:12:36 +0500

    changes to routes

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // Configure routes
-import Home from './home';
-import About from './about';
+import Home from './containers/Home';
+import About from './containers/About';
 import PageNotFound from './common/components/PageNotFound';
 
 export default (
   <Switch>
     <Route exact path="/" component={Home}/>
-    <Route path="/about" component={About}/>
+    <Route exact path="/about" component={About}/>
     <Route component={PageNotFound} />
   </Switch>
 );
