820fb37d6ac59bc2a1207019935c09b1f068f69f
Yasser Zubair <yasserzubair68@gmail.com>
2019-05-11 16:01:30 +0500

    removed comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,9 @@
 import { combineReducers } from 'redux'
 import users from './users'
-// import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
-// import allReducers from './reducers'
 
 export default (history) => combineReducers({
   users,
   router: connectRouter(history),
 });
 
-// const allReducers = combineReducers({
-  
-// })
-
-// export default allReducers;
