3f0a3fcc8304fd99f922642c0ce344f658180ccb
Yasser Zubair <yasserzubair68@gmail.com>
2019-05-11 15:11:02 +0500

    minimum user reducer

---
+++ src/reducers/users.js
@@ -0,0 +1,19 @@
+import { combineReducers } from 'redux'
+import * as types from '../types'
+// import Cookies from 'js-cookie'
+
+const loading = (state = false, action) => {
+  switch(action.type) {
+    case types.API_LOADING:
+      return true
+    case types.API_LOADED:
+      return false
+    default:
+      return state
+  }
+}
+
+export default userReducers = combineReducers({
+  loading,
+})
+
