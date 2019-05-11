7204560f98190882c32a906241fdffd96fba08e2
Yasser Zubair <yasserzubair68@gmail.com>
2019-05-11 15:34:26 +0500

    removed root reducer

--- src/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { connectRouter } from 'connected-react-router'
-import allReducers from './reducers'
-
-export default (history) => combineReducers({
-  router: connectRouter(history),
-  allReducers,
-});
