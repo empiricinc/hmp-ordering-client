4872581fc52824a1e6df92bd69c41878349a43f3
Yasser Zubair <yasserzubair68@gmail.com>
2019-05-18 15:02:27 +0500

    added login toute

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 // Configure routes
 const Home = React.lazy(() => import('./containers/Home'));
 const About = React.lazy(() => import('./containers/About'));
+const Login = React.lazy(() => import('./containers/Login'));
 const PageNotFound = React.lazy(() => import('./containers/PageNotFound'));
 
 export default (
   <Suspense fallback={<div>Loading...</div>}>
     <Switch>
       <Route exact path="/" component={Home}/>
+      <Route exact path="/login" component={Login}/>
       <Route exact path="/about" component={About}/>
       <Route exact path='*' component={PageNotFound} />
     </Switch>
