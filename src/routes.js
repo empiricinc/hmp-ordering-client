e6619a120b85a81ce14b762f64d13f268970f7d0
Yasser Zubair <yasserzubair68@gmail.com>
2019-06-15 14:44:39 +0500

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ import { Route, Switch } from 'react-router-dom';
 const Home = React.lazy(() => import('./containers/Home'));
 const About = React.lazy(() => import('./containers/About'));
 const Login = React.lazy(() => import('./containers/Login'));
+const Documentation = React.lazy(() => import('./containers/Documentation'));
+const Quarantine = React.lazy(() => import('./containers/Quarantine'));
+const Production = React.lazy(() => import('./containers/Production'));
 const PageNotFound = React.lazy(() => import('./containers/PageNotFound'));
 
 export default (
@@ -13,6 +16,9 @@ export default (
       <Route exact path="/" component={Home}/>
       <Route exact path="/login" component={Login}/>
       <Route exact path="/about" component={About}/>
+      <Route exact path="/documentation" component={Documentation}/>
+      <Route exact path="/quarantine" component={Quarantine}/>
+      <Route exact path="/production" component={Production}/>
       <Route exact path='*' component={PageNotFound} />
     </Switch>
   </Suspense>
