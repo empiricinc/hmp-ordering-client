df289d5e95e81c6109d1a014758d31e193c09276
Yasser Zubair <yasserzubair68@gmail.com>
2019-06-15 14:51:25 +0500

    added stcok routes

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ const Login = React.lazy(() => import('./containers/Login'));
 const Documentation = React.lazy(() => import('./containers/Documentation'));
 const Quarantine = React.lazy(() => import('./containers/Quarantine'));
 const Production = React.lazy(() => import('./containers/Production'));
+const Stock = React.lazy(() => import('./containers/Stock'));
 const PageNotFound = React.lazy(() => import('./containers/PageNotFound'));
 
 export default (
@@ -19,6 +20,7 @@ export default (
       <Route exact path="/documentation" component={Documentation}/>
       <Route exact path="/quarantine" component={Quarantine}/>
       <Route exact path="/production" component={Production}/>
+      <Route exact path="/stock" component={Stock}/>
       <Route exact path='*' component={PageNotFound} />
     </Switch>
   </Suspense>
