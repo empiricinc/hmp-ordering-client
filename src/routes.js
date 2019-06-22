2f2b61e09e364d19bba7ca6801cc2c79d26b9910
Yasser Zubair <yasserzubair68@gmail.com>
2019-06-22 17:51:34 +0500

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,25 +2,39 @@ import React, {Suspense} from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // Configure routes
+const Order = React.lazy(() => import('./containers/Order'));
+const OrderDashboard = React.lazy(() => import('./containers/OrderDashboard'));
 const Home = React.lazy(() => import('./containers/Home'));
 const About = React.lazy(() => import('./containers/About'));
 const Login = React.lazy(() => import('./containers/Login'));
 const Documentation = React.lazy(() => import('./containers/Documentation'));
+const DocumentationDashboard = React.lazy(() => import('./containers/DocumentationDashboard'));
 const Quarantine = React.lazy(() => import('./containers/Quarantine'));
+const QuarantineDashboard = React.lazy(() => import('./containers/QuarantineDashboard'));
 const Production = React.lazy(() => import('./containers/Production'));
+const ProductionDashboard = React.lazy(() => import('./containers/ProductionDashboard'));
 const Stock = React.lazy(() => import('./containers/Stock'));
+const StockDashboard = React.lazy(() => import('./containers/StockDashboard'));
+const Signup = React.lazy(() => import('./containers/Signup'));
 const PageNotFound = React.lazy(() => import('./containers/PageNotFound'));
 
 export default (
   <Suspense fallback={<div>Loading...</div>}>
     <Switch>
       <Route exact path="/" component={Home}/>
-      <Route exact path="/login" component={Login}/>
+      <Route exact path="/order" component={Order}/>
+      <Route exact path="/order/dashboard" component={OrderDashboard}/>
+      {/* <Route exact path="/login" component={Login}/> */}
       <Route exact path="/about" component={About}/>
       <Route exact path="/documentation" component={Documentation}/>
+      <Route exact path="/documentation/dashboard" component={DocumentationDashboard}/>
       <Route exact path="/quarantine" component={Quarantine}/>
+      <Route exact path="/quarantine/dashboard" component={QuarantineDashboard}/>
       <Route exact path="/production" component={Production}/>
+      <Route exact path="/production/dashboard" component={ProductionDashboard}/>
+      <Route exact path="/signup" component={Signup}/>
       <Route exact path="/stock" component={Stock}/>
+      <Route exact path="/stock/dashboard" component={StockDashboard}/>
       <Route exact path='*' component={PageNotFound} />
     </Switch>
   </Suspense>
