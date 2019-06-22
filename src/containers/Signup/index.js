302614a5893b27c743bb7f9ac9fe71c957fcecc8
Yasser Zubair <yasserzubair68@gmail.com>
2019-06-22 17:51:27 +0500

    Update index.js

--- src/containers/Signup/index.js
+++ src/containers/Signup/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './style.scss';
 import Form from './Form'
+import config from '../../config'
+import axios from "axios";
+import Swal from 'sweetalert2';
 
 
 class Home extends React.Component {
   handleSubmit = (model) => {
-    console.log(model)
+		console.log(model);
+		axios.post(`${config.apiUrl}/api/users`, model)
+		.then((response) => {
+			Swal.fire({
+				position: 'center',
+				type: 'success',
+				title: 'Sign Up Successful',
+				showConfirmButton: false,
+			})
+			// console.log(response);
+		})
+		.catch((error) => {
+			console.log(error);
+		})
     
   }
 	render() {
