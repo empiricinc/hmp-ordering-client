8cb23768a0f2126a9c3323213149c054ef0d3cfe
Yasser Zubair <yasserzubair68@gmail.com>
2019-06-22 17:51:31 +0500

    Update users.js

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -11,8 +11,26 @@ const loading = (state = false, action) => {
       return state
   }
 }
+const user = (state = null, action) => {
+  switch(action.type) {
+    case types.SET_USER_INFO:
+      return action.payload
+    default:
+      return state
+  }
+}
+const accessiblePaths = (state = [], action) => {
+  switch(action.type) {
+    case types.SET_ACCESSIBLE_PATHS:
+      return action.payload
+    default:
+      return state
+  }
+}
 
 export default combineReducers({
   loading,
+  user,
+  accessiblePaths,
 })
 
