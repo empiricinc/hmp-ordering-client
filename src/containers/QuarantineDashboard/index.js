353262b54ed609f4287da16c3ec6ed0c99a517f9
Yasser Zubair <yasserzubair68@gmail.com>
2019-07-03 19:36:31 +0500

    Update index.js

--- src/containers/QuarantineDashboard/index.js
+++ src/containers/QuarantineDashboard/index.js
@@ -1,37 +1,85 @@
 import React from "react";
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'connected-react-router'
 import { withRouter } from 'react-router-dom';
 // import store, { history } from './configureStore';
 import axios from "axios";
-import Cookies from 'js-cookie'
 import config from "../../config";
-import { setUserInfo } from '../../actions/users'
 import { connect } from "react-redux";
+import moment from 'moment'
+import './style.scss'
 
 
-class Home extends React.Component {
+class QuarantineDashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       authenticating: true,
       authenticated: false,
+      orders: [],
     }
   }
   componentDidMount() {
+    axios.get(`${config.apiUrl}/api/quarantine_dept?status=pending`)
+    .then((response) => {
+      this.setState({
+        orders: response.data
+      })
+    })
   }
   componentWillRecieveProps(nextProps) {
   }
-  render() {
+  addResources(id) {
+    // console.log(this.props);
+    this.props.history.push(`/quaranitneForm/${id}`)
 
-    const { authenticated, authenticating } = this.state;
-    if( authenticated && !authenticating ) {
-      return (
-        <div>Dashboad</div>
-      )
-    } else {
-      return <div>Dashboad</div>
-    }
+  }
+  showDetails(id, department) {
+    // console.log(this.props);
+    this.props.history.push(`/details/${department}/${id}`)
+
+  }
+  completeDocumentation(id) {
+    axios.put(`${config.apiUrl}/api/doc_team/${id}`, {status: 'complete'})
+  }
+  render() {
+    const {orders} = this.state;
+    const {user} = this.props;
+    return(
+    <div class="productionDashWrapper table-responsive table-striped">          
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Product</th>
+        <th>Order Mode</th>
+        <th>Mode of Delivery</th>
+        <th>Date of flight</th>
+        <th>Date of delivery</th>
+        <th>Approved By Manager</th>
+        <th>Resources</th>
+        <th>Complete</th>
+        {user && user.department == 'manager' && <th>Actions</th>}
+      </tr>
+    </thead>
+    <tbody>
+      {
+        orders.map((order, index) => {
+          return <tr>
+            <td>{order.order.product_type}</td>
+            <td>{order.order.mode}</td>
+            <td>{order.order.mode_of_delivery}</td>
+            <td>{moment(order.order.flight_date).format('LL')}</td>
+            <td>{moment(order.order.date_of_delivery).format('LL')}</td>
+            <td>{order.order.isApprove ? 'Yes' : 'No'}</td>
+            <td>{<div><button onClick={() => {this.addResources(order._id)}}>Add/Edit Resources</button></div>}
+            {<div><button onClick={() => {this.showDetails(order._id, 'quaranitne')}}>View Quaranitne Resources</button></div>}</td>
+            {/* {user && user.department == 'manager' && <td><button className='button'>Approve Now</button></td>} */}
+            <td><button onClick={() => {this.completeDocumentation(order._id)}}>Complete From Quarantine</button></td>
+          </tr>
+        })
+      }
+    </tbody>
+  </table>
+  </div>
+  )
   }
 }
 
@@ -40,4 +88,4 @@ export default withRouter(connect(store => {
   return{
     user: store.users.user,
   }
-})(Home))
+})(QuarantineDashboard))
