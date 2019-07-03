3d7411f169347bffd3ccf21919213153c40ecd0c
Yasser Zubair <yasserzubair68@gmail.com>
2019-07-03 19:36:35 +0500

    Create helpers.js

---
+++ src/helpers.js
@@ -0,0 +1,12 @@
+export const productionResources = [
+  {field: 'status', name: 'status'},
+  {field: 'airline_weight', name: 'Airline Weight'},
+  {field: 'loading_weight', name: 'Loading Weight'},
+  {field: 'hot_weight', name: 'Hot Weight'},
+  {field: 'documents_weight', name: 'Weight in Documents'},
+  {field: 'customer_weight', name: 'Weight For Customer'},
+  {field: 'packaging', name: 'Packaging'},
+  {field: 'time', name: 'Time'},
+  {field: 'updatedAt', name: 'Last Updated'},
+  {field: 'vehicle_availability', name: 'Vehicle Availability'},
+]
