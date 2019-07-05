20986ab6fc0f1136c68b131ffa63cdf87e6aa50c
Yasser Zubair <yasserzubair68@gmail.com>
2019-07-06 03:10:23 +0500

    Update index.js

--- src/containers/ProductionDashboard/index.js
+++ src/containers/ProductionDashboard/index.js
@@ -9,6 +9,8 @@ import config from "../../config";
 import { setUserInfo } from '../../actions/users'
 import { connect } from "react-redux";
 import moment from 'moment'
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
 import './style.scss'
 
 
@@ -19,15 +21,26 @@ class ProductionDashboard extends React.Component {
       authenticating: true,
       authenticated: false,
       orders: [],
+      selectedStatus: 'pending',
+      dropdownOpen: false,
     }
   }
   componentDidMount() {
     axios.get(`${config.apiUrl}/api/production_dept?status=pending`)
-    .then((response) => {
-      this.setState({
-        orders: response.data
+      .then((response) => {
+        this.setState({
+          orders: response.data
+        })
       })
-    })
+  }
+  fetchNew = () => {
+    this.setState({ orders: [] })
+    axios.get(`${config.apiUrl}/api/production_dept?status=${this.state.selectedStatus}`)
+      .then((response) => {
+        this.setState({
+          orders: response.data
+        })
+      })
   }
   componentWillRecieveProps(nextProps) {
   }
@@ -42,54 +55,167 @@ class ProductionDashboard extends React.Component {
 
   }
   completeProduction(id) {
-    axios.put(`${config.apiUrl}/api/production_dept/${id}`, {status: 'complete'})
+    axios.put(`${config.apiUrl}/api/production_dept/${id}`, { status: 'completed' })
+      .then(() => {
+        this.setState({ orders: [] }, () => {
+          this.fetchNew()
+        })
+      })
+  }
+  acceptProduction(id) {
+    axios.put(`${config.apiUrl}/api/production_dept/${id}`, { status: 'inProgress' })
+      .then(() => {
+        this.setState({ orders: [] }, () => {
+          this.fetchNew()
+        })
+      })
+  }
+  rejectProduction(id) {
+    axios.put(`${config.apiUrl}/api/production_dept/${id}`, { status: 'rejected' })
+      .then(() => {
+        this.setState({ orders: [] }, () => {
+          this.fetchNew()
+        })
+      })
+  }
+  // rejectProduction(id) {
+  //   axios.put(`${config.apiUrl}/api/production_dept/${id}`, { status: 'rejected' })
+  // }
+  toggle = () => {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+    })
   }
   render() {
-    const {orders} = this.state;
-    const {user} = this.props;
-    return(
-    <div class="productionDashWrapper table-responsive table-striped">          
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Product</th>
-        <th>Order Mode</th>
-        <th>Mode of Delivery</th>
-        <th>Date of flight</th>
-        <th>Date of delivery</th>
-        <th>Approved By Manager</th>
-        <th>Resources</th>
-        <th>Complete</th>
-        {user && user.department == 'manager' && <th>Actions</th>}
-      </tr>
-    </thead>
-    <tbody>
-      {
-        orders.map((order, index) => {
-          return <tr>
-            <td>{order.order.product_type}</td>
-            <td>{order.order.mode}</td>
-            <td>{order.order.mode_of_delivery}</td>
-            <td>{moment(order.order.flight_date).format('LL')}</td>
-            <td>{moment(order.order.date_of_delivery).format('LL')}</td>
-            <td>{order.order.isApprove ? 'Yes' : 'No'}</td>
-            <td>{<div><button onClick={() => {this.addResources(order._id)}}>Add/Edit Resources</button></div>}
-            {<div><button onClick={() => {this.showDetails(order._id, 'production')}}>View Production Resources</button></div>}</td>
-            {/* {user && user.department == 'manager' && <td><button className='button'>Approve Now</button></td>} */}
-            <td><button onClick={() => {this.completeProduction(order._id)}}>Complete From Production</button></td>
-          </tr>
-        })
-      }
-    </tbody>
-  </table>
-  </div>
-  )
+    const { orders, selectedStatus } = this.state;
+    const { user } = this.props;
+    return (
+      <div>
+        <div style={{ height: '75px' }}>
+          <div style={{ width: '100%' }} className='text-center'>
+            <p className='inline-block'>Showing</p>
+            <Dropdown style={{ textAlign: 'center', display: 'inline-block' }} isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+              <DropdownToggle className={'dropdownButton'} caret>
+                {this.state.selectedStatus}
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem onClick={() => { this.setState({ selectedStatus: 'pending' }, () => { this.fetchNew('pending') }) }}>Pending</DropdownItem>
+                <DropdownItem onClick={() => { this.setState({ selectedStatus: 'inProgress' }, () => { this.fetchNew('inProgress') }) }}>In Progress</DropdownItem>
+                <DropdownItem onClick={() => { this.setState({ selectedStatus: 'completed' }, () => { this.fetchNew('completed') }) }}>Completed</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+            <p className='inline-block'>entries</p>
+          </div>
+        </div>
+        {selectedStatus != 'pending' && <div class="productionDashWrapper table-responsive table-striped">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Order Mode</th>
+                <th>Carcass Weight</th>
+                <th>Mode of Delivery</th>
+                <th>Date of flight</th>
+                <th>Date of delivery</th>
+                <th>Approved By Manager</th>
+                <th>Resources</th>
+                {/* <th>Complete</th> */}
+                {/* <th>Reject</th> */}
+                {/* {user && user.department == 'manager' && <th>Actions</th>} */}
+              </tr>
+            </thead>
+            <tbody>
+              {
+                orders.length ? orders.map((order, index) => {
+                  return <tr>
+                    <td>{order.order.product_type}</td>
+                    <td>{order.order.mode}</td>
+                    <td>{order.order.carcase_weight}</td>
+                    <td>{order.order.mode_of_delivery}</td>
+                    <td>{moment(order.order.flight_date).format('LL')}</td>
+                    <td>{moment(order.order.date_of_delivery).format('LL')}</td>
+                    <td>{order.order.isApprove ? 'Yes' : 'No'}</td>
+                    <td>
+                      {
+                        <div>
+                          <button onClick={() => { this.addResources(order._id) }}>Add/Edit Resources</button>
+                        </div>
+                      }
+                      {
+                        <div>
+                          <button onClick={() => { this.showDetails(order._id, 'production') }}>View Production Resources</button>
+                        </div>
+                      }
+                      
+                      {
+                        <div>
+                          <button onClick={() => { this.showDetails(order.order.quarantine_team, 'quarantine') }}>View Quarantine Resources</button>
+                        </div>
+                      }
+                      {
+                        <div>
+                          <button onClick={() => { this.showDetails(order.order.documentation_team, 'documentation') }}>View Documentation Resources</button>
+                        </div>
+                      }
+                    </td>
+                    {/* {user && user.department == 'manager' && <td><button className='button'>Approve Now</button></td>} */}
+                    {/* <td><button onClick={() => { this.completeProduction(order._id) }}>Complete From Production</button></td> */}
+                    {/* <td><button onClick={() => { this.rejectProduction(order._id) }}>Reject</button></td> */}
+                  </tr>
+                }) : <tr><td colSpan='8'>No Entries Found</td></tr>
+              }
+            </tbody>
+          </table>
+        </div>
+        }
+        {
+          selectedStatus == 'pending' &&
+          <div className="productionDashWrapper table-responsive table-striped">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Order Mode</th>
+                  <th>Carcass Weight</th>
+                  <th>Mode of Delivery</th>
+                  <th>Date of flight</th>
+                  <th>Date of delivery</th>
+                  <th>Accept</th>
+                  <th>Reject</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  orders.length ? orders.map((order, index) => {
+                    return <tr>
+                      <td>{order.order.product_type}</td>
+                      <td>{order.order.mode}</td>
+                      <td>{order.order.carcase_weight}</td>
+                      <td>{order.order.mode_of_delivery}</td>
+                      <td>{moment(order.order.flight_date).format('LL')}</td>
+                      <td>{moment(order.order.date_of_delivery).format('LL')}</td>
+                      <td><button onClick={() => { this.acceptProduction(order._id) }}>Accept</button></td>
+                      {/* <td>{order.order.isApprove ? 'Yes' : 'No'}</td> */}
+                      {/* <td>{<div><button onClick={() => { this.addResources(order._id) }}>Add/Edit Resources</button></div>} */}
+                      {/* {<div><button onClick={() => { this.showDetails(order._id, 'documentation') }}>View Documentation Resources</button></div>}</td> */}
+                      {/* {user && user.department == 'manager' && <td><button className='button'>Approve Now</button></td>} */}
+                      {/* <td><button onClick={() => { this.completeDocumentation(order._id) }}>Complete From Documentation</button></td> */}
+                      <td><button onClick={() => { this.rejectProduction(order._id) }}>Reject</button></td>
+                    </tr>
+                  }) : <tr><td colSpan='8'>No Entries Found</td></tr>
+                }
+              </tbody>
+            </table>
+          </div>
+        }
+      </div>
+    )
   }
 }
 
 
 export default withRouter(connect(store => {
-  return{
+  return {
     user: store.users.user,
   }
 })(ProductionDashboard))
