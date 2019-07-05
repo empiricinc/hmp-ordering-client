9cd5933c1c24604cd5cd994cb35430b0db0d9733
Yasser Zubair <yasserzubair68@gmail.com>
2019-07-06 03:09:46 +0500

    Update index.js

--- src/containers/DetailsContainer/index.js
+++ src/containers/DetailsContainer/index.js
@@ -5,6 +5,19 @@ import { connect } from 'react-redux'
 import {withRouter} from 'react-router-dom'
 import './style.scss'
 
+function validURL(str) {
+  if(typeof(str) == 'string') {
+      var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+  }else {
+    return false
+  }
+}
 
 class Details extends Component {
   constructor(props) {
@@ -22,9 +35,17 @@ class Details extends Component {
     this.department = match.params.department;
     const deptUrls = {
       production: 'production_dept',
+      quarantine: 'quarantine_dept',
+      documentation: 'doc_team',
+      stock: 'stock',
     }
     axios.get(`${config.apiUrl}/api/${deptUrls[this.department]}/${this.entityId}`)
     .then((response) => {
+      delete response.data.__v
+      delete response.data._id
+      delete response.data.updatedAt
+      delete response.data.createdAt
+      response.data.vehicle_availability = response.data.vehicle_availability === true ? 'Yes' : 'NO' 
       this.setState({details: response.data})
     })
   }
@@ -41,11 +62,29 @@ class Details extends Component {
                   <h5 className='heading text-capitalize'>{name.split('_').join(' ')}</h5>
                 </div>
                 <div>
-                  <p className='detail'>{details[name]}</p>
+                  <p className='detail'>{details[name] && validURL(details[name]) ? <a target="_blank" href={details[name]}> Download File</a> : details[name] }</p>
                 </div>
               </React.Fragment>
             )
           })
+        }
+        {
+          this.department && this.department == 'stock' ? 
+          details.animals_ref && details.animals_ref.length && details.animals_ref.map((item, index) => {
+            // <div></div>
+            console.log(item)
+            return Object.keys(item).map((entry, index) => {
+              return <div>
+                <div>
+                  <h6 className='heading text-capitalize'>{entry.split('_').join(' ')}</h6>
+                </div>
+                <div>
+                  <p className='detail'>{item[entry]}</p>
+                </div>
+              </div>
+            })
+          })
+          : null
         }
       </div>
     )
