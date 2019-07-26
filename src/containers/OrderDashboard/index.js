740854a2810b2c93bbd5d5229c0fc42369442009
Yasser Zubair <yasserzubair68@gmail.com>
2019-07-26 20:25:04 +0500

    formatting

--- src/containers/OrderDashboard/index.js
+++ src/containers/OrderDashboard/index.js
@@ -23,79 +23,79 @@ class Home extends React.Component {
   }
   componentDidMount() {
     axios.get(`${config.apiUrl}/api/order?isApprove=true`)
-    .then((response) => {
-      this.setState({
-        orders: response.data
+      .then((response) => {
+        this.setState({
+          orders: response.data
+        })
       })
-    })
   }
-  fetchNew () {
+  fetchNew() {
     axios.get(`${config.apiUrl}/api/order?isApprove=true`)
-    .then((response) => {
-      this.setState({
-        orders: response.data
+      .then((response) => {
+        this.setState({
+          orders: response.data
+        })
       })
-    })
   }
   componentWillRecieveProps(nextProps) {
   }
   approveOrder(id) {
     axios.put(`${config.apiUrl}/api/order/approve/${id}`)
-    .then((response) => {
-      window.alert('Order has been approved')
-      this.fetchNew();
-    })
+      .then((response) => {
+        window.alert('Order has been approved')
+        this.fetchNew();
+      })
   }
   rejectOrder(id) {
     axios.put(`${config.apiUrl}/api/order/approve/${id}`)
-    .then((response) => {
-      window.alert('Order has been approved')
-      this.fetchNew();
-    })
+      .then((response) => {
+        window.alert('Order has been approved')
+        this.fetchNew();
+      })
   }
   render() {
-    const {orders} = this.state;
-    const {user} = this.props;
-    return(
-    <div class="orderDashWrapper  table-responsive table-striped">          
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Product</th>
-        <th>Order Mode</th>
-        <th>Carcass Weight</th>
-        <th>Mode of Delivery</th>
-        <th>Date of flight</th>
-        <th>Date of delivery</th>
-        <th>Production Status</th>
-        <th>Documentation Status</th>
-        <th>Quarantine Status</th>
-        {/* <th>Approved</th> */}
-        {/* {user && user.department == 'manager' && <th>Actions</th>} */}
-      </tr>
-    </thead>
-    <tbody>
-      {
-        orders.map((order, index) => {
-          return <tr>
-            <td>{
-                      order.order_items.map((item,index) => {
-                        return <div>
-                          <h6>{item.item}</h6>
-                          <p>{item.carcass_weight} KGs</p>
-                        </div>
+    const { orders } = this.state;
+    const { user } = this.props;
+    return (
+      <div class="orderDashWrapper  table-responsive table-striped">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Order Mode</th>
+              <th>Carcass Weight</th>
+              <th>Mode of Delivery</th>
+              <th>Date of flight</th>
+              <th>Date of delivery</th>
+              <th>Production Status</th>
+              <th>Documentation Status</th>
+              <th>Quarantine Status</th>
+              {/* <th>Approved</th> */}
+              {/* {user && user.department == 'manager' && <th>Actions</th>} */}
+            </tr>
+          </thead>
+          <tbody>
+            {
+              orders.map((order, index) => {
+                return <tr>
+                  <td>{
+                    order.order_items.map((item, index) => {
+                      return <div>
+                        <h6>{item.item}</h6>
+                        <p>{item.carcass_weight} KGs</p>
+                      </div>
                     })
-                    }</td>
-            <td>{order.mode}</td>
-            <td>{order.carcase_weight}</td>
-            <td>{order.mode_of_delivery}</td>
-            <td>{moment(order.flight_date).format('LL')}</td>
-            <td>{moment(order.date_of_delivery).format('LL')}</td>
-            <td>{order.production_team.status}</td>
-            <td>{order.documentation_team.status}</td>
-            <td>{order.quarantine_team.status}</td>
-            {/* <td>{order.isApprove ? 'Yes' : 'No'}</td> */}
-            {/* {user && user.department == 'manager' && 
+                  }</td>
+                  <td>{order.mode}</td>
+                  <td>{order.carcase_weight}</td>
+                  <td>{order.mode_of_delivery}</td>
+                  <td>{moment(order.flight_date).format('LL')}</td>
+                  <td>{moment(order.date_of_delivery).format('LL')}</td>
+                  <td>{order.production_team.status}</td>
+                  <td>{order.documentation_team.status}</td>
+                  <td>{order.quarantine_team.status}</td>
+                  {/* <td>{order.isApprove ? 'Yes' : 'No'}</td> */}
+                  {/* {user && user.department == 'manager' && 
             <td>
               <button onClick={ () => {this.approveOrder(order._id)} } className='button'>
                 Approve 
@@ -105,19 +105,19 @@ class Home extends React.Component {
               </button>
             </td>
             } */}
-          </tr>
-        })
-      }
-    </tbody>
-  </table>
-  </div>
-  )
+                </tr>
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    )
   }
 }
 
 
 export default withRouter(connect(store => {
-  return{
+  return {
     user: store.users.user,
   }
 })(Home))
